e345223619ee0db6897376a7df80268c88b1ff38
Adam Paszke <apaszke@google.com>
2020-05-05 15:34:09 +0000

    RPC without overlapping

--- src/lib/PipeRPC.hs
+++ src/lib/PipeRPC.hs
@@ -4,7 +4,19 @@
 -- license that can be found in the LICENSE file or at
 -- https://developers.google.com/open-source/licenses/bsd
 
-module PipeRPC (PipeServer, startPipeServer, callPipeServer, psPop) where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module PipeRPC (PipeServer, startPipeServer, callPipeServer) where
 
 import Control.Concurrent.MVar
 import Control.Monad
@@ -15,27 +27,44 @@ import GHC.IO.Handle.FD
 import System.IO
 import System.Process
 
-data PipeServer f = PipeServer { _psLock          :: MVar ()
-                               , _psSendHandle    :: Handle
-                               , _psReceiveHandle :: Handle
-                               , psFunctionIndex :: Int}
+data PipeServer (methods :: [*])
+ = PipeServer { _psLock          :: MVar ()
+              , _psSendHandle    :: Handle
+              , _psReceiveHandle :: Handle }
 
-startPipeServer :: MonadIO m => FilePath -> [String] -> m (PipeServer f)
+startPipeServer :: MonadIO m => FilePath -> [String] -> m (PipeServer methods)
 startPipeServer cmd args = liftIO $ do
   ((clientRead, _), (_, serverWrite)) <- createPipeWithNames
   ((_, serverRead), (clientWrite, _)) <- createPipeWithNames
   void $ createProcess $ proc cmd $ args ++ [serverRead, serverWrite]
   lock <- newMVar ()
-  return $ PipeServer lock clientWrite clientRead 0
+  return $ PipeServer lock clientWrite clientRead
 
-psPop :: PipeServer (head, tail) -> PipeServer tail
-psPop server = server { psFunctionIndex = 1 + psFunctionIndex server }
+class Member elem (list :: [*]) where
+  index :: Int
 
-callPipeServer :: (MonadIO m, ToJSON a, FromJSON b)
-               => PipeServer (a -> b, tail) -> a -> m b
-callPipeServer (PipeServer lock sendHandle receiveHandle fIdx) arg = liftIO $ do
+instance Member' elem list (IsHead elem list) => Member elem list where
+  index = index' @elem @list @(IsHead elem list)
+
+class Member' elem (list :: [*]) (atHead :: Bool) where
+  index' :: Int
+
+instance Member' h (h ': t) 'True where
+  index' = 0
+
+instance Member' e t (IsHead e t) => Member' e (h ': t) 'False where
+  index' = 1 + index' @e @t @(IsHead e t)
+
+type family IsHead elem (list :: [*]) :: Bool where
+  IsHead h (h ': _) = 'True
+  IsHead h _        = 'False
+
+callPipeServer :: forall a b methods m
+               .  (MonadIO m, ToJSON a, FromJSON b, Member (a -> b) methods)
+               => PipeServer methods -> a -> m b
+callPipeServer (PipeServer lock sendHandle receiveHandle) arg = liftIO $ do
   void $ takeMVar lock
-  let request = unpack $ encode (fIdx, arg)
+  let request = unpack $ encode (index @(a -> b) @methods, arg)
   hPutStrLn sendHandle request
   response <- hGetLine receiveHandle
   putMVar lock ()
